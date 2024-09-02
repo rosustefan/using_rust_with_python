c8d656a99edd88cbe7884f89d9a502ccb2d1d2a1
Ștefan Roșu <rosu.stefan@hotmail.com>
2024-09-02 08:24:56 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+# [Using Python with Rust](https://www.linkedin.com/learning/using-rust-with-python/pyo3-installation?u=2165786)
---
+++ venv/lib/python3.10/site-packages/pyo3_example/__init__.py
@@ -0,0 +1,5 @@
+from .pyo3_example import *
+
+__doc__ = pyo3_example.__doc__
+if hasattr(pyo3_example, "__all__"):
+    __all__ = pyo3_example.__all__
