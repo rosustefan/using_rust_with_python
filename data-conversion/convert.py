081bcafaaac55feaaff8a7c386bfd44398a72eef
Ștefan Roșu <rosu.stefan@hotmail.com>
2024-09-02 11:40:43 +0000

    Add data-conversion example project

---
+++ data-conversion/convert.py
@@ -0,0 +1,9 @@
+#!/home/ubuntu/bin/rust-scripts/using_rust_with_python/venv/bin/python3
+
+import libdata_conversion
+
+
+data_types = libdata_conversion.data_types_example()
+
+print(data_types)  # Output: {'text': 'Hello, Python!', 'integer': 42, 'floating': 3.14, 'boolean': True}
+
